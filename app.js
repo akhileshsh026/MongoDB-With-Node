23a6cdc92eb12df0870bbf92756978d8e95b55ef
Akhilesh Kumar Sahu <akhileshsh026@hotmail.com>
2018-06-11 19:21:46 +0530

    Donw With mongo Driver bu tthe Syntax has been changed  like added url , client etc.

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
-var express = require('express'),
-  cons = require('consolidate'),
-  mongodb = require('mongodb');
+var MongoClient = require('mongodb').MongoClient ,
+   assert  = require('assert');
+
+   const url = 'mongodb+srv://Akhileshsh026:Akhilesh-123@cluster0-rsbn2.gcp.mongodb.net';
+    
+   MongoClient.connect(url,function(err, client) {
+
+        assert.equal(null,err);
+        console.log('sucesfully connected to server');
+
+
+        const db = client.db('videos');
+        // Find Some documents in our Server
+        db.collection('movies').find({}).toArray(function(err,docs) {
+          
+          // print title of movies
+           docs.forEach(function(doc) {
+             console.log(doc.title);
+           });
+               client.close();
+        });
+
+        console.log('Colling the Find()');
+   });
