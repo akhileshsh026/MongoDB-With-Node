8b0440839564c10d68163a6414537bfc9e6ef4dc
Akhilesh Kumar Sahu <akhileshsh026@hotmail.com>
2018-08-14 18:01:31 +0530

    movies templete modified for ul  list and , db.collections error in app1.js due to incompatibility

--- app1.js
+++ app1.js
@@ -1,22 +1,36 @@
 var experss = require('express'),
    app = experss(),
-   engines = require('consolidate');
+   engines = require('consolidate'),
+   MongoClient = require('mongodb').MongoClient,
+   assert = require('assert');
 
-
+   const url = 'mongodb+srv://Akhileshsh026:Akhilesh-123@cluster0-rsbn2.gcp.mongodb.net';
+    
    app.engine('html', engines.nunjucks);
    app.set('view engine','html');
    app.set('views',__dirname + '/views');
 
-   app.get('/' , function(req,res) {
-       res.render('hello',{'name':'templetes'});   // hello is templete stored in /views folder
-   });
+MongoClient.connect(url,(err,db) => {
+    
+    assert.equal(null,err);
+    console.log('Sucessfully connected to server');
 
-   app.use(function(req,res) {
-       res.sendStatus(404);
-   });
+    app.get('/' , function(req,res) {
+        db.collection('movies').find({}).toArray(function(err, docs) {
+            res.render('movie',{'movies':docs});
+        });
+    });
 
+    app.use(function(req,res) {
+        res.sendStatus(404);
+    });
+
+    
    var server = app.listen('9000',function() {
-       var port = server.address().port;
-       console.log('Server is listing at port no : ',port);
+    var port = server.address().port;
+    console.log('Server is listing at port no : ',port);
+   });
 
-   })
+});
+
+
